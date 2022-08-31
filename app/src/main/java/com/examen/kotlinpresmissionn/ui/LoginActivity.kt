dc146ca3635ab52d347639c758e9bff13392cce4
Sainathhiwale <sainathhiwale2@gmail.com>
2022-08-31 08:03:38 -0400

    Simple Retrofit coroutine 1.4: login activity is added

---
+++ app/src/main/java/com/examen/kotlinpresmissionn/ui/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.examen.kotlinpresmissionn.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.examen.kotlinpresmissionn.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
